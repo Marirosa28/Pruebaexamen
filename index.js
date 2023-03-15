5b47849a404693b8b0c0b0af5a23cc405024d210
Mary <121901648+Marirosa28@users.noreply.github.com>
2023-03-15 22:33:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+let correctas = [4];
+
+let opcion_elegida =[];
+
+let cantidad_correctas=0;
+
+function respuesta(num_respuesta, seleccionada){
+  opcion_elegida[num_pregunta] = seleccionada.value;
+
+  id="p" + num_pregunta;
+
+  labels = document.getElementById(id).childNodes;
+  labels[3].style.backgroundColor = "white";
+  labels[5].style.backgroundColor = "white";
+  labels[7].style.backgroundColor = "white";
+
+   seleccionada.parentNode.style.backgroundColor = "#cec0fc";
+}
+
+function corregir(){
+  cantidad_correctas = 0;
+  for(i=0; i < correctas.length;i++){
+    if(correctas[i]==opcion_elegida[i]){ 
+    cantidad_correctas++;
+  }
+}
+document.getElementById("resultado").innerHTML = cantidad_correctas;
+   }
